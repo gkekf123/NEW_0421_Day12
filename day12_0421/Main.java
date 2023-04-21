40e88b72df66d9695273952428d820588955bf07
TAEJU LEE <92421686+gkekf123@users.noreply.github.com>
2023-04-21 11:09:56 +0900

    상속

---
+++ day12_0421/Main.java
@@ -0,0 +1,26 @@
+package day12_0421;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		Warrior war = new Warrior("전사 박인욱");
+		
+		war.info();
+		war.bash();
+		
+		System.out.println();
+		
+		
+		Wizard wiz = new Wizard("법사 박인욱");
+		
+		wiz.info();
+		wiz.iceArrow();
+		
+		
+
+		
+	}
+
+}
---
+++ day12_0421/Player.java
@@ -0,0 +1,16 @@
+package day12_0421;
+
+public class Player {
+	
+	String name;
+	int hp;
+	int mp;
+	
+	void info() {
+		System.out.println("케릭명:" + name);
+		System.out.println("hp:" + hp);
+		System.out.println("mp:" + mp);
+	}
+	
+	
+}
